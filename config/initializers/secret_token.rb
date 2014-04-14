eee6337bb74bb6772afe068da8bcdfbc33d620bf
zhangyanan <zhangyanan@huama.com.cn>
2014-04-14 14:27:38 +0800

    commit the initialize data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OaSys::Application.config.secret_token = 'b64d02170407fc89baded6982ab6dd6c5de73077bf4b3a30abcaef119f585182a824b7393f1d5ecba7668ea5f1b1a75c07c1520e2b7b7eca7a4a116b01a30f81'
